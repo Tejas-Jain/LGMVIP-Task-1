7d6f591017d231d32f99db9f22caceeca7bbf3da
Tejas Jain <1tejasjain@gmail.com>
2022-12-27 18:59:37 +0530

    Temp Change

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-// Create a "close" button and append it to each list item
 var myNodelist = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
@@ -9,8 +7,6 @@ for (i = 0; i < myNodelist.length; i++) {
     span.appendChild(txt);
     myNodelist[i].appendChild(span);
 }
-
-// Click on a close button to hide the current list item
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
@@ -19,16 +15,12 @@ for (i = 0; i < close.length; i++) {
         div.style.display = "none";
     }
 }
-
-// Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ul');
 list.addEventListener('click', function (ev) {
     if (ev.target.tagName === 'LI') {
         ev.target.classList.toggle('checked');
     }
 }, false);
-
-// Create a new list item when clicking on the "Add" button
 function newElement() {
     var li = document.createElement("li");
     var inputValue = document.getElementById("myInput").value;
